6173dcd28e95839c9ecff0ae64a3a665cfa02e3b
Țeculescu Octavian-Alexandru <octav_teculescu@yahoo.com>
2020-04-17 14:21:18 +0300

    Added parametrized easing functions factory

--- src/main/lib/utils/easing.go
+++ src/main/lib/utils/easing.go
@@ -3,6 +3,10 @@ package utils
 type EasingFunction func(t float64) float64
 
 // ease in functions
+func EaseInLinear(t float64) float64 {
+	return t
+}
+
 func EaseInQuad(t float64) float64 {
 	return t * t
 }
@@ -16,6 +20,10 @@ func EaseInQuart(t float64) float64 {
 }
 
 // ease out functions
+func EaseOutLinear(t float64) float64 {
+	return 1 - t
+}
+
 func EaseOutQuad(t float64) float64 {
 	return 1 - EaseInQuad(1-t)
 }
@@ -50,5 +58,44 @@ func EaseInOutQuart(t float64) float64 {
 		return EaseInQuart(2*t) / 2
 	} else {
 		return EaseOutQuart(2*(t-0.5)) / 2
+	}
+}
+
+// composite functions
+func Step(x float64) EasingFunction {
+	return func(t float64) float64 {
+		if t < x {
+			return 0
+		} else {
+			return 1
+		}
+	}
+}
+
+func Spike(x float64, f EasingFunction) EasingFunction {
+	fx1 := 1.0 / x
+	fx2 := 1.0 / (1.0 - x)
+	return func(t float64) float64 {
+		if t < x {
+			return f(fx1 * t)
+		} else {
+			return 1 - f(fx2*(t-x))
+		}
+	}
+}
+
+func MirrorSpike(f EasingFunction) EasingFunction {
+	return Spike(0.5, f)
+}
+
+func Wave(x float64, f EasingFunction) EasingFunction {
+	fx1 := 1.0 / x
+	fx2 := 1.0 / (1.0 - x)
+	return func(t float64) float64 {
+		if t < x {
+			return f(fx1 * t)
+		} else {
+			return f(1 - (fx2 * (t - x)))
+		}
 	}
 }
