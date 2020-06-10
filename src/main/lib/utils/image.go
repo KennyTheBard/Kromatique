d9b0dc81e94965d7ca3a898585ebafe0759bb3b0
Țeculescu Octavian-Alexandru <octav_teculescu@yahoo.com>
2020-06-10 14:08:02 +0300

    Removed imageio functions from utils

--- src/main/lib/utils/image.go
+++ src/main/lib/utils/image.go
@@ -1,49 +1,10 @@
 package utils
 
 import (
-	"errors"
 	"image"
 	"image/color"
 	"image/draw"
-	"image/jpeg"
-	"image/png"
-	"os"
-	"strings"
 )
-
-// Load returns an image object of the file located at given path
-func Load(path string) (image.Image, error) {
-	file, errOpen := os.Open(path)
-	if errOpen != nil {
-		return nil, errOpen
-	}
-	defer file.Close()
-
-	img, _, errDecode := image.Decode(file)
-	if errDecode != nil {
-		return nil, errDecode
-	}
-	return img, nil
-}
-
-// Save write the given image in a file of chosen format at the given path
-func Save(img image.Image, path, format string) error {
-	file, errCreate := os.Create(strings.Join([]string{path, format}, "."))
-	if errCreate != nil {
-		return errCreate
-	}
-	defer file.Close()
-
-	switch format {
-	case "png":
-		return png.Encode(file, img)
-
-	case "jpeg", "jpg":
-		return jpeg.Encode(file, img, &jpeg.Options{Quality: jpeg.DefaultQuality})
-	}
-
-	return errors.New("Unsupported file format: " + format)
-}
 
 // CreateRGBA returns an image with the given size
 func CreateRGBA(bounds image.Rectangle) image.Image {
